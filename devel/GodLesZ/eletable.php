40e06108fa0b1dae4ac2c71cca34ff3669787164
GodLesZ <GodLesZ@dfcd82a3-dffb-8f83-6aec-effe02521c5c>
2011-12-14 11:14:57 +0000

    attr_fix.txt update (bugreport:5119)

---
+++ devel/GodLesZ/eletable.php
@@ -0,0 +1,69 @@
+<?php
+
+	$data = '<table class="wikitablec elements"><caption> Level 1</caption><tr><th></th><th> Neutral</th><th> Water</th><th> Earth</th><th> Fire</th><th> Wind</th><th> Poison</th><th> Holy</th><th> Shadow</th><th> Ghost</th><th> Undead</th></tr><tr><th> Neutral</th><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td class="ineffective"> 70%</td><td> 100%</td></tr><tr><th> Water</th><td> 100%</td><td class="ineffective"> 25%</td><td> 100%</td><td class="ineffective"> 90%</td><td class="effective"> 175%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td></tr><tr><th> Earth</th><td> 100%</td><td> 100%</td><td class="ineffective"> 25%</td><td class="effective"> 150%</td><td class="ineffective"> 90%</td><td class="effective"> 125%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td></tr><tr><th> Fire</th><td> 100%</td><td class="effective"> 150%</td><td class="ineffective"> 90%</td><td class="ineffective"> 25%</td><td> 100%</td><td class="effective"> 125%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td></tr><tr><th> Wind</th><td> 100%</td><td class="ineffective"> 90%</td><td class="effective"> 150%</td><td> 100%</td><td class="ineffective"> 25%</td><td class="effective"> 125%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td></tr><tr><th> Poison</th><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td class="nullified"> 0%</td><td> 100%</td><td class="ineffective"> 50%</td><td> 100%</td><td class="ineffective"> 50%</td></tr><tr><th> Holy</th><td> 100%</td><td class="ineffective"> 75%</td><td class="ineffective"> 75%</td><td class="ineffective"> 75%</td><td class="ineffective"> 75%</td><td class="ineffective"> 75%</td><td class="nullified"> 0%</td><td class="effective"> 125%</td><td class="ineffective"> 75%</td><td> 100%</td></tr><tr><th> Shadow</th><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td class="ineffective"> 50%</td><td class="effective"> 125%</td><td class="nullified"> 0%</td><td class="ineffective"> 75%</td><td class="nullified"> 0%</td></tr><tr><th> Ghost</th><td class="ineffective"> 70%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td class="effective"> 125%</td><td> 100%</td></tr><tr><th> Undead</th><td> 100%</td><td> 100%</td><td> 100%</td><td class="effective"> 125%</td><td> 100%</td><td class="nullified"> -25%</td><td class="effective"> 150%</td><td class="nullified"> -25%</td><td> 100%</td><td class="nullified"> 0%</td></tr></table><table class="wikitablec elements"><caption> Level 2</caption><tr><th></th><th> Neutral</th><th> Water</th><th> Earth</th><th> Fire</th><th> Wind</th><th> Poison</th><th> Holy</th><th> Shadow</th><th> Ghost</th><th> Undead</th></tr><tr><th> Neutral</th><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td class="ineffective"> 50%</td><td> 100%</td></tr><tr><th> Water</th><td> 100%</td><td class="nullified"> 0%</td><td> 100%</td><td class="ineffective"> 80%</td><td class="effective"> 175%</td><td class="ineffective"> 75%</td><td> 100%</td><td> 100%</td><td class="ineffective"> 75%</td><td class="ineffective"> 75%</td></tr><tr><th> Earth</th><td> 100%</td><td> 100%</td><td class="nullified"> 0%</td><td class="effective"> 175%</td><td class="ineffective"> 80%</td><td class="effective"> 125%</td><td> 100%</td><td> 100%</td><td class="ineffective"> 75%</td><td class="ineffective"> 75%</td></tr><tr><th> Fire</th><td> 100%</td><td class="effective"> 175%</td><td class="ineffective"> 90%</td><td class="nullified"> 0%</td><td> 100%</td><td class="effective"> 125%</td><td> 100%</td><td> 100%</td><td class="ineffective"> 75%</td><td class="ineffective"> 75%</td></tr><tr><th> Wind</th><td> 100%</td><td class="ineffective"> 80%</td><td class="effective"> 175%</td><td> 100%</td><td class="nullified"> 0%</td><td class="effective"> 125%</td><td> 100%</td><td> 100%</td><td class="ineffective"> 75%</td><td class="ineffective"> 75%</td></tr><tr><th> Poison</th><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td class="nullified"> 0%</td><td> 100%</td><td class="ineffective"> 25%</td><td class="ineffective"> 75%</td><td class="ineffective"> 25%</td></tr><tr><th> Holy</th><td> 100%</td><td class="ineffective"> 50%</td><td class="ineffective"> 50%</td><td class="ineffective"> 50%</td><td class="ineffective"> 50%</td><td class="ineffective"> 50%</td><td class="nullified"> -25%</td><td class="effective"> 150%</td><td class="ineffective"> 50%</td><td class="effective"> 125%</td></tr><tr><th> Shadow</th><td> 100%</td><td class="ineffective"> 75%</td><td class="ineffective"> 75%</td><td class="ineffective"> 75%</td><td class="ineffective"> 75%</td><td class="ineffective"> 25%</td><td class="effective"> 150%</td><td class="nullified"> -25%</td><td class="ineffective"> 50%</td><td class="nullified"> 0%</td></tr><tr><th> Ghost</th><td class="ineffective"> 50%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td class="ineffective"> 75%</td><td> 100%</td><td> 100%</td><td class="effective"> 150%</td><td> 100%</td></tr><tr><th> Undead</th><td> 100%</td><td> 100%</td><td> 100%</td><td class="effective"> 150%</td><td> 100%</td><td class="nullified"> -50%</td><td class="effective"> 175%</td><td class="nullified"> -50%</td><td class="effective"> 125%</td><td class="nullified"> 0%</td></tr></table><table class="wikitablec elements"><caption> Level 3</caption><tr><th></th><th> Neutral</th><th> Water</th><th> Earth</th><th> Fire</th><th> Wind</th><th> Poison</th><th> Holy</th><th> Shadow</th><th> Ghost</th><th> Undead</th></tr><tr><th> Neutral</th><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td class="nullified"> 0%</td><td> 100%</td></tr><tr><th> Water</th><td> 100%</td><td class="nullified"> -25%</td><td> 100%</td><td class="ineffective"> 70%</td><td class="effective"> 200%</td><td class="ineffective"> 50%</td><td> 100%</td><td> 100%</td><td class="ineffective"> 50%</td><td class="ineffective"> 50%</td></tr><tr><th> Earth</th><td> 100%</td><td> 100%</td><td class="nullified"> -25%</td><td class="effective"> 200%</td><td class="ineffective"> 70%</td><td> 100%</td><td> 100%</td><td> 100%</td><td class="ineffective"> 50%</td><td class="ineffective"> 50%</td></tr><tr><th> Fire</th><td> 100%</td><td class="effective"> 200%</td><td class="ineffective"> 70%</td><td class="nullified"> -25%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td class="ineffective"> 50%</td><td class="ineffective"> 50%</td></tr><tr><th> Wind</th><td> 100%</td><td class="ineffective"> 70%</td><td class="effective"> 200%</td><td> 100%</td><td class="nullified"> -25%</td><td> 100%</td><td> 100%</td><td> 100%</td><td class="ineffective"> 50%</td><td class="ineffective"> 50%</td></tr><tr><th> Poison</th><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td class="nullified"> 0%</td><td class="effective"> 125%</td><td class="nullified"> 0%</td><td class="ineffective"> 50%</td><td class="nullified"> 0%</td></tr><tr><th> Holy</th><td> 100%</td><td class="ineffective"> 25%</td><td class="ineffective"> 25%</td><td class="ineffective"> 25%</td><td class="ineffective"> 25%</td><td class="ineffective"> 25%</td><td class="nullified"> -50%</td><td class="effective"> 175%</td><td class="ineffective"> 25%</td><td class="effective"> 150%</td></tr><tr><th> Shadow</th><td> 100%</td><td class="ineffective"> 50%</td><td class="ineffective"> 50%</td><td class="ineffective"> 50%</td><td class="ineffective"> 50%</td><td class="nullified"> 0%</td><td class="effective"> 175%</td><td class="nullified"> -50%</td><td class="ineffective"> 25%</td><td class="nullified"> 0%</td></tr><tr><th> Ghost</th><td class="nullified"> 0%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td class="ineffective"> 50%</td><td> 100%</td><td> 100%</td><td class="effective"> 175%</td><td> 100%</td></tr><tr><th> Undead</th><td> 100%</td><td class="effective"> 125%</td><td> 100%</td><td class="effective"> 175%</td><td> 100%</td><td class="nullified"> -75%</td><td class="effective"> 200%</td><td class="nullified"> -75%</td><td class="effective"> 150%</td><td class="nullified"> 0%</td></tr></table><table class="wikitablec elements"><caption> Level 4</caption><tr><th></th><th> Neutral</th><th> Water</th><th> Earth</th><th> Fire</th><th> Wind</th><th> Poison</th><th> Holy</th><th> Shadow</th><th> Ghost</th><th> Undead</th></tr><tr><th> Neutral</th><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td class="nullified"> 0%</td><td> 100%</td></tr><tr><th> Water</th><td> 100%</td><td class="nullified"> -50%</td><td> 100%</td><td class="ineffective"> 60%</td><td class="effective"> 200%</td><td class="ineffective"> 25%</td><td class="ineffective"> 75%</td><td class="ineffective"> 75%</td><td class="ineffective"> 25%</td><td class="ineffective"> 25%</td></tr><tr><th> Earth</th><td> 100%</td><td> 100%</td><td class="nullified"> -50%</td><td class="effective"> 200%</td><td class="ineffective"> 60%</td><td class="ineffective"> 75%</td><td class="ineffective"> 75%</td><td class="ineffective"> 75%</td><td class="ineffective"> 25%</td><td class="ineffective"> 25%</td></tr><tr><th> Fire</th><td> 100%</td><td class="effective"> 200%</td><td class="ineffective"> 60%</td><td class="nullified"> -50%</td><td> 100%</td><td class="ineffective"> 75%</td><td class="ineffective"> 75%</td><td class="ineffective"> 75%</td><td class="ineffective"> 25%</td><td class="ineffective"> 25%</td></tr><tr><th> Wind</th><td> 100%</td><td class="ineffective"> 60%</td><td class="effective"> 200%</td><td> 100%</td><td class="nullified"> -50%</td><td class="ineffective"> 75%</td><td class="ineffective"> 75%</td><td class="ineffective"> 75%</td><td class="ineffective"> 25%</td><td class="ineffective"> 25%</td></tr><tr><th> Poison</th><td> 100%</td><td class="ineffective"> 75%</td><td class="ineffective"> 75%</td><td class="ineffective"> 75%</td><td class="ineffective"> 75%</td><td class="nullified"> 0%</td><td class="effective"> 125%</td><td class="nullified"> -25%</td><td class="ineffective"> 25%</td><td class="nullified"> -25%</td></tr><tr><th> Holy</th><td> 100%</td><td class="nullified"> 0%</td><td class="nullified"> 0%</td><td class="nullified"> 0%</td><td class="nullified"> 0%</td><td class="nullified"> 0%</td><td class="nullified"> -100%</td><td class="effective"> 200%</td><td class="nullified"> 0%</td><td class="effective"> 175%</td></tr><tr><th> Shadow</th><td> 100%</td><td class="ineffective"> 25%</td><td class="ineffective"> 25%</td><td class="ineffective"> 25%</td><td class="ineffective"> 25%</td><td class="nullified"> -25%</td><td class="effective"> 200%</td><td class="nullified"> -100%</td><td class="nullified"> 0%</td><td class="nullified"> 0%</td></tr><tr><th> Ghost</th><td class="nullified"> 0%</td><td> 100%</td><td> 100%</td><td> 100%</td><td> 100%</td><td class="ineffective"> 25%</td><td> 100%</td><td> 100%</td><td class="effective"> 200%</td><td> 100%</td></tr><tr><th> Undead</th><td> 100%</td><td class="effective"> 150%</td><td class="ineffective"> 50%</td><td class="effective"> 200%</td><td> 100%</td><td class="nullified"> -100%</td><td class="effective"> 200%</td><td class="nullified"> -100%</td><td class="effective"> 175%</td><td class="nullified"> 0%</td></tr></table>';
+	$nl = '<br />';
+
+	$doc = new DOMDocument();
+	$doc->loadHTML($data);
+
+	$_names = array(
+		'Neutral',
+		'Water',
+		'Earth',
+		'Fire',
+		'Wind',
+		'Poison',
+		'Holy',
+		'Shadow',
+		'Ghost',
+		'Undead'
+	);
+	$elements_all = array();
+
+	foreach ($doc->getElementsByTagName('table') as $table) {
+		$elements = array();
+
+		foreach ($table->getElementsByTagName('tr') as $iRow => $row) {
+			//echo '<br />' . $iRow . ': ';
+			$cols = $row->getElementsByTagName('td');
+
+			foreach ($cols as $i => $col) {
+				//echo $col->nodeValue . ',';
+
+				$ele_atk = $i;
+				$ele_def = $iRow - 1;
+				$elements[$_names[$ele_atk]][$_names[$ele_def]] = trim(str_replace('%', '', $col->nodeValue));
+			}
+		}
+
+		$elements_all[] = $elements;
+	}
+
+
+	$out = '';
+	foreach ($elements_all as $level => $elements) {
+		$out .= ($level + 1) . ',' . count($elements) . "\t" . '// lv' . ($level + 1) . ' Attribute table' . $nl;
+		$out .= '//Neut Watr Erth Fire Wind Pois Holy Shdw Gho  Und' . $nl;
+
+		foreach ($elements as $ele_attack_name => $row) {
+			$out .= ' ';
+
+			foreach ($row as $value) {
+				$out .= str_pad($value, 4, ' ', STR_PAD_LEFT) . ',';
+			}
+			
+			$out .= '  // ' . $ele_attack_name . $nl;
+		}
+
+		$out .= $nl;
+	}
+
+	echo '<pre>';
+	echo $out;
+	print_r($elements_all);
+	echo '</pre>';
+
+	echo '<br />';
+	echo $doc->saveHTML();
+
+?>
