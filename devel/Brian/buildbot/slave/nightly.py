7fcd0f357162b9ae3b000758dea565a226f873a2
flaviojs <flaviojs@dfcd82a3-dffb-8f83-6aec-effe02521c5c>
2012-04-23 02:38:53 +0000

    Wait until everything is done before requesting shutdown.

--- devel/Brian/buildbot/slave/nightly.py
+++ devel/Brian/buildbot/slave/nightly.py
@@ -2,17 +2,41 @@ from buildslave.bot import BuildSlave
 from twisted.internet.task import LoopingCall
 
 class NightlyBuildSlave(BuildSlave):
-    """Tells the master, a minute after starting, that it wants to shutdown when all builds are done.
-    You must pass allow_shutdown='nightly' in the contructor to enable this behaviour.
+    """Slave that builds all pending jobs and then shuts down.
+
+    It checks every minute if a command is running. If not active in 2 
+    consecutive checks, it assumes everything is done and tells the master
+    that it wants to shutdown.
+
+    You must pass allow_shutdown='nightly' in the contructor to enable this
+    behaviour.
+
     Requires buildbot 0.8.3+ in the master and in the slave."""
 
     def startService(self):
         BuildSlave.startService(self)
         if self.allow_shutdown == 'nightly':
-            self.shutdown_loop = l = LoopingCall(self._callGracefulShutdown)
-            l.start(interval=60,now=False) # wait for a minute
+            self.shutdown_loop = l = LoopingCall(self._checkShutdownNightly)
+            l.start(interval=60,now=False) # check every minute
+            self.last_active = False
 
-    def _callGracefulShutdown(self):
-        self.gracefulShutdown()
-        self.shutdown_loop.stop()
-        self.shutdown_loop = None
+    def _checkShutdownNightly(self):
+        if self._active():
+            self.last_active = True # active
+        elif self.last_active:
+            self.last_active = False # no longer active
+        else: # not active for a minute
+            self.gracefulShutdown()
+            self.shutdown_loop.stop()
+            self.shutdown_loop = None
+            self.last_active = None
+
+    def _active(self):
+        try:
+            bot = self.getServiceNamed("bot")
+            for builder in bot.builders.values():
+                if builder.command:
+                    return True # running a command, active
+        except KeyError:
+            pass
+        return False
