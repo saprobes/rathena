312c72e8d2ad7270c8a1025005a4bb1496918558
flaviojs <flaviojs@dfcd82a3-dffb-8f83-6aec-effe02521c5c>
2012-04-28 20:11:10 +0000

    Make NightlyBuildSlave ignore checks done while the slave isn't connected.

--- devel/Brian/buildbot/slave/nightly.py
+++ devel/Brian/buildbot/slave/nightly.py
@@ -6,30 +6,35 @@ class NightlyBuildSlave(BuildSlave):
 
     It checks every minute if a command is running. If not active in 2 
     consecutive checks, it assumes everything is done and tells the master
-    that it wants to shutdown.
+    that it wants to shutdown. Checks done while the slave isn't connected
+    are ignored.
 
     You must pass allow_shutdown='nightly' in the contructor to enable this
     behaviour.
 
     Requires buildbot 0.8.3+ in the master and in the slave."""
 
+    inactive_shutdown = 2 # number of consecutive checks needed to shutdown
+
     def startService(self):
         BuildSlave.startService(self)
         if self.allow_shutdown == 'nightly':
             self.shutdown_loop = l = LoopingCall(self._checkShutdownNightly)
-            l.start(interval=60,now=False) # check every minute
-            self.last_active = False
+            l.start(interval=60,now=True) # check every minute
+            self.inactive_count = 0
 
     def _checkShutdownNightly(self):
+        if not self.bf.perspective:
+            return # no connection
         if self._active():
-            self.last_active = True # active
-        elif self.last_active:
-            self.last_active = False # no longer active
-        else: # not active for a minute
+            self.inactive_count = 0 # active
+            return
+        self.inactive_count += 1
+        if self.inactive_count >= self.inactive_shutdown:
             self.gracefulShutdown()
             self.shutdown_loop.stop()
             self.shutdown_loop = None
-            self.last_active = None
+            self.inactive_count = None
 
     def _active(self):
         try:
