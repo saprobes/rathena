30d97933758938f0d5af3ab2bd4fe35f9738390f
flaviojs <flaviojs@dfcd82a3-dffb-8f83-6aec-effe02521c5c>
2012-05-12 07:53:47 +0000

    NightlyBuildSlave: automatically shut down if not connected during 5 consecutive checks

--- devel/Brian/buildbot/slave/nightly.py
+++ devel/Brian/buildbot/slave/nightly.py
@@ -4,37 +4,47 @@ from twisted.internet.task import LoopingCall
 class NightlyBuildSlave(BuildSlave):
     """Slave that builds all pending jobs and then shuts down.
 
-    It checks every minute if a command is running. If not active in 2 
-    consecutive checks, it assumes everything is done and tells the master
-    that it wants to shutdown. Checks done while the slave isn't connected
-    are ignored.
+    You must pass allow_shutdown='nightly' in the contructor to enable this behaviour:
 
-    You must pass allow_shutdown='nightly' in the contructor to enable this
-    behaviour.
+    It checks every minute if it's connected and if a command is running (active).
+
+    If not connected during 'noconnection_shutdown' consecutive checks,
+    it automatically shuts down.
+
+    If not active during 'inactive_shutdown' consecutive checks (while connected),
+    it assumes everything is done and tells the master that it wants to shutdown.
 
     Requires buildbot 0.8.3+ in the master and in the slave."""
 
-    inactive_shutdown = 2 # number of consecutive checks needed to shutdown
+    noconnection_shutdown = 5 # number of consectutive noconnection checks needed to shutdown
+    inactive_shutdown = 2 # number of consectutive inactive checks needed to shutdown (while connected)
 
     def startService(self):
         BuildSlave.startService(self)
         if self.allow_shutdown == 'nightly':
             self.shutdown_loop = l = LoopingCall(self._checkShutdownNightly)
-            l.start(interval=60,now=True) # check every minute
+            self.noconnection_count = 0
             self.inactive_count = 0
+            l.start(interval=60,now=True) # check every minute
 
     def _checkShutdownNightly(self):
-        if not self.bf.perspective:
-            return # no connection
-        if self._active():
-            self.inactive_count = 0 # active
+        if not self.bf.perspective:# no connection
+            self.noconnection_count += 1
+            if self.noconnection_count < self.noconnection_shutdown:
+                return
+        self.noconnection_count = 0
+        if self._active():# active
+            self.inactive_count = 0
             return
-        self.inactive_count += 1
-        if self.inactive_count >= self.inactive_shutdown:
-            self.gracefulShutdown()
-            self.shutdown_loop.stop()
-            self.shutdown_loop = None
-            self.inactive_count = None
+        else:# inactive
+            self.inactive_count += 1
+            if self.inactive_count < self.inactive_shutdown:
+                return
+        self.gracefulShutdown()
+        self.shutdown_loop.stop()
+        self.shutdown_loop = None
+        self.noconnection_count = None
+        self.inactive_count = None
 
     def _active(self):
         try:
